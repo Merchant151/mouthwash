76d96b331a9bbecc571de2cebb5840f8ac704e41
Ahkeelah <ahkeelah.lindo@gmail.com>
2025-12-28 13:26:12 -0500

    commit

--- dialogue.py
+++ dialogue.py
@@ -1,2 +1,23 @@
-print('Welcome to Mouthwash, please enter in the items you are accoustum to getting weekly.')
+import shopping_list
+
+Welcome = 'Welcome to Mouthwash, please enter in the items you are accustomed to getting weekly.'; 
+WelcomeBack = 'Welcome back to Mouthwash, your current list is ' + 'userlist'
+ToAdd = 'To add an item select + and type in each item you would like to add seperated by coma.'
+ToRemove = 'To remove press - select the number of the item on the list you would like to remove, if several items seperate with coma.'
+ToAlter = 'To alter just type * and the number of the item you would like to edit.'
+Options = 'Please take a look at our menu options' + ' ' + ToAdd + ' ' + ToRemove + ' ' + ToAlter 
+YouAdded = 'You added ' + 'listItemfuction' + ' to list'
+YouRemoved = 'You removed ' + 'listItemfuction' + ' from your list'
+
+
+
+
+if list == None:
+  myList.create('My Shopping List')
+  print(Welcome)
+  print(ToAdd)
+  if list != None:
+    println(WelcomeBack)
+    println(Options)
+
 
